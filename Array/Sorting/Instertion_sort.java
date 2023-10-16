7bcabf6db144c15d0302701e59a6244f444a6e2d
ShriRam <sriramsingh289@gmail.com>
2023-10-16 22:18:20 +0530

    insertion sort

---
+++ Array/Sorting/Instertion_sort.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Instertion_sort {
+    
+    public static void main(String[] args) {
+        
+        int[] arr = {3,2,1,4,5};
+        insertion(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println("Terminated Successfully");
+    }
+
+
+    // defination of the insertion sort
+    static void insertion(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for( int j = i+1 ; j > 0; j--){
+                if(arr[j]<arr[j-1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }else {
+                    break;
+                }
+            }
+        }
+    }
+
+}
